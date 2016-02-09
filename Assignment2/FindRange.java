befe2fa12b1d495d2e584f8ec93cc42c04f3a0b4
Mike Tyan <mike.tyan@yahoo.com>
2016-02-09 00:19:34 -0800

    Finished
    Final changes made to complete assignment.

--- Assignment2/FindRange.java
+++ Assignment2/FindRange.java
@@ -3,7 +3,9 @@
  * ---------------------
  * Program will find the largest and smallest integers entered.
  * -One Integer entered at a time, using '0' as SENTINEL to end list.
- * -Largest and smallest number will be displayed.
+ * -Largest and smallest number will be set as the first value entered.
+ * -Entering SENTINEL on first entry will display message stating 'no values
+ * have been entered'.
  */
 
 import acm.program.*;
@@ -19,25 +21,29 @@ public class FindRange extends ConsoleProgram {
 		println ("the end of the list.");
 		//Sets initial value for 'n1' with user input.
 		int n = readInt (" ? ");
-		//Sets value 'n' as both the largest and smallest values.
-		int highN = n;
-		int lowN = n;
-		//Loop until 'n' equals 'SENTINEL'.
-		while (true) {
-			n = readInt (" ? ");
-			if (n == SENTINEL) break; 
-			//Determines if entered value is larger than the largest value or 
-			//smaller than the smallest value. If either is true, then the current 
-			//value is replaced with new value.
-			if (n > highN) {
-				highN = n;
-			} else {
-				if (n < lowN) {
-					lowN = n;
-				}
-			}	
+		if (n == SENTINEL) {
+			println("No values have been entered.");
+		} else {
+			//Sets value 'n' as both the largest and smallest values.
+			int highN = n;
+			int lowN = n;
+			//Loop until 'n' equals 'SENTINEL'.
+			while (true) {
+				n = readInt (" ? ");
+				if (n == SENTINEL) break; 
+				//Determines if entered value is larger than the largest value or 
+				//smaller than the smallest value. If either is true, then the current 
+				//value is replaced with new value.
+				if (n > highN) {
+					highN = n;
+				} else {
+					if (n < lowN) {
+						lowN = n;
+					}
+				}	
+			}
+			println ("The largest integer is " + highN + ".");
+			println ("The smallest integer is " + lowN + ".");
 		}
-		println ("The largest integer is " + highN + ".");
-		println ("The smallest integer is " + lowN + ".");
 	}
 }
