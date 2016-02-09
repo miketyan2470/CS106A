3bb6489309dc04a1a841365d4da6fb3cf0675729
Mike Tyan <mike.tyan@yahoo.com>
2016-02-09 00:13:03 -0800

    Uploaded

---
+++ Assignment2/FindRange.java
@@ -0,0 +1,43 @@
+/*
+ * File : FindRange.java
+ * ---------------------
+ * Program will find the largest and smallest integers entered.
+ * -One Integer entered at a time, using '0' as SENTINEL to end list.
+ * -Largest and smallest number will be displayed.
+ */
+
+import acm.program.*;
+
+public class FindRange extends ConsoleProgram {
+	//Sets constant SENTINEL to '0'.
+	private static final int SENTINEL = 0;
+
+	public void run() {
+		println ("This program finds the largest and smallest integer in a given list.");
+		println ();
+		println ("Enter values, one per line, using a '0' to signal");
+		println ("the end of the list.");
+		//Sets initial value for 'n1' with user input.
+		int n = readInt (" ? ");
+		//Sets value 'n' as both the largest and smallest values.
+		int highN = n;
+		int lowN = n;
+		//Loop until 'n' equals 'SENTINEL'.
+		while (true) {
+			n = readInt (" ? ");
+			if (n == SENTINEL) break; 
+			//Determines if entered value is larger than the largest value or 
+			//smaller than the smallest value. If either is true, then the current 
+			//value is replaced with new value.
+			if (n > highN) {
+				highN = n;
+			} else {
+				if (n < lowN) {
+					lowN = n;
+				}
+			}	
+		}
+		println ("The largest integer is " + highN + ".");
+		println ("The smallest integer is " + lowN + ".");
+	}
+}
